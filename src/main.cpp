0025bb7eb513deffe308de72334e86ba5b548b69
powerof3 <powerof3@users.noreply.github.com>
2024-10-02 04:15:31 +0530

    recompile for SF 1.14.70.0+

--- src/main.cpp
+++ src/main.cpp
@@ -4,41 +4,42 @@ namespace Handler
 {
 	struct GetMaxCarryWeight
 	{
-		static float func(RE::Actor* a_actor, bool a_useGravity)
+		static float func(RE::Actor* a_actor)
 		{
 			auto carryWeight = a_actor->GetActorValue(*RE::ActorValue::GetSingleton()->carryWeight);
-			if (a_useGravity) {
-				auto localGravity = a_actor->GetGravityScale();
-				if (localGravity < 0.1f) {
-					localGravity = 0.1f;
-				}
-
-				// higher grav = less carry weight, lower grav = more carry weight
-				carryWeight *= Settings::GetSingleton()->baseGravity / localGravity;
+			
+			auto localGravity = a_actor->GetGravityScale();
+			if (localGravity < 0.1f) {
+				localGravity = 0.1f;
 			}
+
+			// higher grav = less carry weight, lower grav = more carry weight
+			carryWeight *= Settings::GetSingleton()->baseGravity / localGravity;
+			
 			RE::BGSEntryPoint::HandleEntryPoint(RE::BGSEntryPoint::ENTRY_POINT::kGetMaxCarryWeight, a_actor, std::addressof(carryWeight));
+			
 			return carryWeight;
 		}
-		static inline constexpr std::size_t size{ 0xBA };
+		static inline constexpr std::size_t size{ 0x5A };
 	};
 
 	struct AdjustItemWeight
 	{
-		static float thunk(RE::Actor* a_this, std::uintptr_t a_inventoryStack, float a_itemWeight)
+		static float thunk(RE::Actor* a_this, const RE::BGSInventoryItem& a_invItem, float a_weight, bool* a_shouldModifyWholeStackOut)
 		{
-			if (a_itemWeight > 0.0f) {
+			if (a_weight > 0.0f) {
 				auto localGravity = a_this->GetGravityScale();
 				if (localGravity < 0.1f) {
 					localGravity = 0.1f;
 				}
 
 				// higher grav = weigh more, lower grav = weight less
-			    float itemWeight = a_itemWeight * localGravity / Settings::GetSingleton()->baseGravity;
+				float itemWeight = a_weight * localGravity / Settings::GetSingleton()->baseGravity;
 
-			    return func(a_this, a_inventoryStack, itemWeight);
+				return func(a_this, a_invItem, itemWeight, a_shouldModifyWholeStackOut);
 			}
 
-			return func(a_this, a_inventoryStack, a_itemWeight);
+			return func(a_this, a_invItem, a_weight, a_shouldModifyWholeStackOut);
 		}
 		static inline REL::Relocation<decltype(thunk)> func;
 		static inline std::size_t                      idx = 0x99;
@@ -52,7 +53,7 @@ namespace Handler
 		if (settings->adjustItemWeights) {
 			stl::write_vfunc<AdjustItemWeight>(RE::VTABLE::PlayerCharacter[38]);
 		} else {
-			REL::Relocation<std::uintptr_t> target{ REL::ID(150799) };
+			REL::Relocation<std::uintptr_t> target{ REL::ID(1721804) };
 			stl::asm_replace<GetMaxCarryWeight>(target.address());
 		}
 	}
@@ -76,9 +77,7 @@ DLLEXPORT constinit auto SFSEPlugin_Version = []() noexcept {
 	data.PluginName(Version::PROJECT);
 	data.AuthorName("powerofthree");
 	data.UsesAddressLibrary(true);
-	//data.UsesSigScanning(true);
 	data.IsLayoutDependent(true);
-	//data.HasNoStructUse(true);
 	data.CompatibleVersions({ SFSE::RUNTIME_LATEST });
 
 	return data;
@@ -87,6 +86,9 @@ DLLEXPORT constinit auto SFSEPlugin_Version = []() noexcept {
 DLLEXPORT bool SFSEAPI SFSEPlugin_Load(const SFSE::LoadInterface* a_sfse)
 {
 	SFSE::Init(a_sfse);
+
+	logger::info("Game version : {}", a_sfse->RuntimeVersion());
+	logger::info("Plugin version : {}", Version::NAME);
 
 	SFSE::GetMessagingInterface()->RegisterListener(MessageCallback);
 
